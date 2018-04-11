59c4e1fd21558e64fba3b47f886cef59d759c35e
weihan.hwh <weihan.hwh@alibaba-inc.com>
2018-04-11 20:24:43 +0800

    recurrent

--- house/spiders/house.py
+++ house/spiders/house.py
@@ -3,21 +3,24 @@ import scrapy
 
 class HouseSpider(scrapy.Spider):
     name = "house"
-    start_urls = [
-        'https://gz.lianjia.com/ershoufang/pg1/',
-    ]
+
+    def start_requests(self):
+        start_url = 'https://gz.lianjia.com/ershoufang/pg'
+
+        for i in range(1, 2):
+            yield scrapy.Request(url=start_url + str(i), callback=self.parse)
 
     def parse(self, response):
         for info in response.css("li.clear"):
             yield {
-                'title': info.css("div.title > a::text").extract(),
-                'price': info.css("div.totalPrice > span::text").extract()
+                'title': info.css("div.title > a::text").extract_first(),
+                'price': info.css("div.totalPrice > span::text").extract_first()
             }
         yield {
             'test': 'test1'
         }
-        for url in self.start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        # url = self.start_urls
+        # yield scrapy.Request(url=url, callback=self.parse)
         # next_page_url = response.css("li.next > a::attr(href)").extract_first()
         # if next_page_url is not None:
         #     yield scrapy.Request(response.urljoin(next_page_url))
