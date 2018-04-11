e8b2b4627afc9edb67b3293402121abd60c84f32
wayne.foks@gmail.com <4hv&1989>
2018-04-11 22:12:38 +0800

    encode

--- house/spiders/house.py
+++ house/spiders/house.py
@@ -7,18 +7,17 @@ class HouseSpider(scrapy.Spider):
     def start_requests(self):
         start_url = 'https://gz.lianjia.com/ershoufang/pg'
 
-        for i in range(1, 2):
+        for i in range(1, 3):
             yield scrapy.Request(url=start_url + str(i), callback=self.parse)
 
     def parse(self, response):
         for info in response.css("li.clear"):
             yield {
                 'title': info.css("div.title > a::text").extract_first(),
-                'price': info.css("div.totalPrice > span::text").extract_first()
+                'price': info.css("div.totalPrice > span::text").extract_first(),
+                'follow': info.css("div.followInfo::text").extract_first()
             }
-        yield {
-            'test': 'test1'
-        }
+
         # url = self.start_urls
         # yield scrapy.Request(url=url, callback=self.parse)
         # next_page_url = response.css("li.next > a::attr(href)").extract_first()
