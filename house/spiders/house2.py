f2005b09da9a4a2dabfc843a5c278552d95b9d3f
wayne.foks@gmail.com <4hv&1989>
2018-04-14 18:43:56 +0800

    house2

--- house/spiders/house2.py
+++ house/spiders/house2.py
@@ -3,7 +3,7 @@ import scrapy
 
 # 在house工程目录下执行
 # cd house
-# scrapy crawl house -s JOBDIR=./crawls/somespider-1
+# scrapy crawl house2 -s JOBDIR=./crawls/somespider-1
 
 class HouseSpider2(scrapy.Spider):
     name = "house2"
@@ -40,10 +40,10 @@ class HouseSpider2(scrapy.Spider):
         yield {
             'number': self.number,
             '出售价': extract_with_css('span.dealTotalPrice > i::text').extract_first().strip(),
-            '挂牌价': extract_with_css('div.msg').css('span > label::text').extract()[0].strip(),
+            '挂牌价': getValueSafely(extract_with_css('div.msg').css('span > label::text').extract(), 0).strip(),
             '单价': extract_with_css('div.price > b::text').extract_first().strip(),
             '行政区': getValueSafely(extract_with_css('div.deal-bread > a::text').extract(), 2).replace('二手房成交价格', ''),
-            '小区': extract_with_css('div.house-title > div::text').extract_first().split(' ')[0].strip(),
+            '小区': getValueSafely(extract_with_css('div.house-title > div::text').extract_first().split(' '), 0).strip(),
             '建筑面积': getValueSafely(
                 response.css('div.introContent').css('div.content').css('li::text')[2].re('(\d*\.*\d*)'), 0).strip(),
             '建筑类型': response.css('div.introContent').css('div.content').css('li::text')[5].extract().strip(),
@@ -51,10 +51,10 @@ class HouseSpider2(scrapy.Spider):
             '朝向': response.css('div.introContent').css('div.content').css('li::text')[6].extract().strip(),
             '户型2': response.css('div.introContent').css('div.content').css('li::text')[0].extract().strip(),
             '楼层': response.css('div.introContent').css('div.content').css('li::text')[1].extract().strip(),
-            '所在楼层': response.css('div.introContent').css('div.content').css('li::text')[1].extract().split("(")[
-                0].strip(),
-            '总楼层': response.css('div.introContent').css('div.content').css('li::text')[1].extract().split("(")[
-                1].strip(),
+            '所在楼层': getValueSafely(
+                response.css('div.introContent').css('div.content').css('li::text')[1].extract().split("("), 0).strip(),
+            '总楼层': getValueSafely(response.css('div.introContent').css('div.content').css('li::text')[1].re('共(\d*)层'),
+                                  0).strip(),
             '装修': response.css('div.introContent').css('div.content').css('li::text')[8].extract().strip(),
             '梯户比': response.css('div.introContent').css('div.content').css('li::text')[11].extract().strip(),
             '电梯': response.css('div.introContent').css('div.content').css('li::text')[13].extract().strip(),
